ea702564fd7a8d1d74a1bda960c31ec0cc3c3be7
Nguyen Minh Tuan <tnowad@gmail.com>
2026-04-29 16:57:37 +0700

    feat(auth): add OauthController and routes
    - authorize, callback, disconnect, repos, orgs endpoints
    - OAuth state param for CSRF protection
    - Token stored on callback, cleared on disconnect
    - Routes: /oauth/authorize, /oauth/callback, /oauth/disconnect,
      /oauth/repos, /oauth/orgs
    
    refs RM-23, refs #1

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@
 
 namespace :redmine_github do
   post '/:repository_id/webhook', as: 'webhook', to: 'webhooks#dispatch_event'
+
+  scope :oauth do
+    get  'authorize',   as: 'oauth_authorize',   to: 'oauth#authorize'
+    get  'callback',    as: 'oauth_callback',     to: 'oauth#callback'
+    post 'disconnect',  as: 'oauth_disconnect',   to: 'oauth#disconnect'
+  end
+
+  get 'repos', as: 'repos', to: 'oauth#repos'
+  get 'orgs',  as: 'orgs',  to: 'oauth#orgs'
 end
